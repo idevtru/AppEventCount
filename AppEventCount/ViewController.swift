6180a04839f18ff38fdaa04c0c9656fef507a6b0
Rich Castagna <idev.tru@gmail.com>
2019-01-29 00:20:16 -0500

    Initial Commit

---
+++ AppEventCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppEventCount
+//
+//  Created by Richard Castagna on 1/29/19.
+//  Copyright © 2019 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
